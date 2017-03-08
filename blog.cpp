0c85c9b0bfeb435f9a47f67306e506798b8f4ecd
luisfbgs <arueluis@gmail.com>
2017-03-07 23:04:05 -0300

    Update blog.cpp

--- blog.cpp
+++ blog.cpp
@@ -3,16 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define LOGIN   1
-#define SIGNUP  2
-#define VISITOR 3
-#define EXIT    4
+#define LOGIN   '1'
+#define SIGNUP  '2'
+#define VISITOR '3'
+#define EXIT    '4'
+#define PASS_E  4
+#define MAIL_E  3
+#define NICK_E  2
 
 #define NICKNAME_SIZE_LIMIT 20
 #define PASSWORD_SIZE 5
 #define PASSWORD_REPETICION_LIMIT 1
 
+#ifdef _WIN32
+#define CLEAR "cls"
+#else
+#define CLEAR "clear"
+#endif
+
+
 class User{
+public:
 	string nickname;
 	string password;
 	string email;
@@ -31,7 +42,7 @@ class User{
 		}
 		
 		for(auto c : u_nickname){
-			if(!alfa(c)){
+			if(!isalpha(c)){
 				return false;
 			}
 		}
@@ -44,16 +55,13 @@ class User{
 			return false;
 		}
 		
+		map<char,int> repeat;
+
 		for(auto c : u_password){
-			int cnt = 0;
+			
+			repeat[c]++;
 
-			for(auto h : u_password){
-				if(c == h){
-					cnt++;
-				}
-			}
-
-			if(cnt > PASSWORD_REPETICION_LIMIT){
+			if(repeat[c] > PASSWORD_REPETICION_LIMIT){
 				return false;
 			}
 		}
@@ -65,8 +73,23 @@ class User{
 	//TODO: do this in an efficient way
 	//TODO: test later with a lot of cases
 	bool valid_email(string u_email){
-
+		char valid[3];
+		int i = 0;
+		valid[0] = '@';
+		valid[1] = '.';
+		valid[2] = 'a';
+		for(auto c : u_email){
+			if(!isalpha(c)){
+				if(c != valid[i])
+					return false;
+				else
+					i++;
+			}
+		}
 		return true;
+	}
+	void anonymous_user(){
+		anonymous = true;
 	}
 };
 
@@ -89,7 +112,7 @@ class Post{
 	//TODO: verify problems with this implementation
 	//TODO: verify if the grade send if between 1 and 5
 	void add_grade(int grade){
-		grades.insert(grade);
+		grades.push_back(grade);
 	}
 
 	int final_grade(){
@@ -101,54 +124,120 @@ class Post{
 
 		return grade / grades.size();
 	}
-}
+};
+
+map<string,User> users_informations; //OBTER TODOS OS DADOS DO USUARIO APARTIR DO NOME 
 
 //TODO: print this message red
-void error_message(bool error){
-	if(error){
-		printf("invalid option");
-	}
+void error_message(int error){
+	if(error == 1)
+		cout << "Invalid option\n";
+	else if(error == NICK_E)
+		cout << "The username must contain only letters and at most 20 characters\n";
+	else if(error == PASS_E)
+		cout << "The password must contain 5 characters whitout repetition\n";
+	else if(error == MAIL_E)
+		cout << "The e-mail must contain only letters in the format: paradinhas@paradinhas.paradnhas\n";
+
 }
 
-void login(){
-	bool error = false;
+void render_start_page(){
+	system(CLEAR);
+	cout << "Escolha uma opcao bacana\n";
+	cout << "1 - Sing in.\n";
+	cout << "2 - Sing up.\n";
+	cout << "3 - Visitant mode.\n";
+	cout << "4 - EXIT.\n";
+}
+
+void render_signup_page(){
+	system(CLEAR);
+
+	string nick, password, email;
+	User new_user(nick,password,email);
+
+	cout << "Username: ";
+	cin >> nick;
+	while(!new_user.valid_nickname(nick)){
+		system(CLEAR);
+		error_message(NICK_E);
+		cout << "Username: ";
+		cin >> nick;
+	}
+
+	system(CLEAR);
+
+	cout << "Password: ";
+	cin >> password;
+	while(!new_user.valid_password(password)){
+		system(CLEAR);
+		error_message(PASS_E);
+		cout << "Password: ";
+		cin >> password;
+	}
+
+	system(CLEAR);
+
+	cout << "E-mail: ";
+	cin >> email;
+	while(!new_user.valid_email(email)){
+		system(CLEAR);
+		error_message(MAIL_E);
+		cout << "E-mail: ";
+		cin >> email;
+	}
+
+	system(CLEAR);
+
+	User valid_user(nick,password,email);
+	//users_informations[nick] = valid_user;
+	cout << "Congratulations " << valid_user.nickname << "! You are now a member of the blog BLOG!\n";
+	cout << "Press ENTER to return to the initial menu and Sing In\n";
+	getchar();
+	getchar();
+
+}
+
+void initial_menu(){
+	int error = 0;
 
 	while(true){
 		char op;
 		
 		render_start_page();
 		error_message(error);
-		get_input(op);
+		error = 0;
+		cin >> op;
+		//get_input(op);
 
 		if(op == LOGIN){
-			render_login_page();
+			//render_login_page();
 		}
 		else if(op == SIGNUP){
 			render_signup_page();
 		}
 		else if(op == VISITOR){
-			User current_user = User.anonymous_user();
-			render_initial_page(current_user);
+			User current_user("0","9","1");
+			current_user.anonymous_user();
+			//render_initial_page(current_user);
 		}
-		else if(op ==  EXIT){
+		else if(op ==  EXIT)
 			break;
-		}
-		else{
-			error == true;
-		}
+		else
+			error = 1;
 	}
 }
 
-void set_db(){
-	if(db_exist()){
-		read_db();
-	}
-	else{
-		create_db();
-	}
-}
+// void set_db(){
+// 	if(db_exist()){
+// 		read_db();
+// 	}
+// 	else{
+// 		create_db();
+// 	}
+// }
 
 int main(){
-	set_db(); // le os arquivos
-	login();
+	//set_db(); // le os arquivos
+	initial_menu();
 }
