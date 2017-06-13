1edc0116f106da4f893df8f4628e35a0b9339e43
claudiosegala <claudiosegalafilho@gmail.com>
2017-06-13 11:56:54 -0300

    documentation of view updated

--- blog4.0/view.hpp
+++ blog4.0/view.hpp
@@ -25,48 +25,48 @@
  */
 class BlogView {
 
-friend class BlogController;
+  friend class BlogController;
 
-private:
-	static const int LIST = 1;
-	static const int MYBLOGS = 2;
-	
-	static void list_blogs_page();
-	static void my_blogs_page();
-	/*
-	 * Render field page
-	 * Just print 'Preencha seu nome:' and return
-	 */
-	Name field_name_page();
-	/*
-	 * Render field page
-	 * Just print 'Preencha seu email:' and return
-	 */
-	Email field_email_page();
-	
-public:
-	/*
-	 * Render create blog page
-	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-	 */
-	void create_page();
-	/*
-	 * Render show page
-	 * Receives the id of blog and a boolean (if boolean true, show all blogs)
-	 */
-	static void show_page(bool);
-	/*
-	 * Render edit profile page
-	 * Receives the id of blog
-	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-	 */
-	void edit_page(unsigned int);
-	/*
-	 * Render edit profile page
-	 * Receives the id of blog
-	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-	 */
-	void delete_page(unsigned int);
+  private:
+    static const int LIST = 1;
+    static const int MYBLOGS = 2;
+    
+    static void list_blogs_page();
+    static void my_blogs_page();
+    /*
+     * Render field page
+     * Just print 'Preencha seu nome:' and return
+     */
+    Name field_name_page();
+    /*
+     * Render field page
+     * Just print 'Preencha seu email:' and return
+     */
+    Email field_email_page();
+    
+  public:
+    /*
+     * Render create blog page
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void create_page();
+    /*
+     * Render show page
+     * Receives the id of blog and a boolean (if boolean true, show all blogs)
+     */
+    static void show_page(bool);
+    /*
+     * Render edit profile page
+     * Receives the id of blog
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void edit_page(unsigned int);
+    /*
+     * Render edit profile page
+     * Receives the id of blog
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void delete_page(unsigned int);
 };
 
 //------------------------------------------------
@@ -82,38 +82,38 @@ public:
  */
 class CommentView {
 
-friend class CommentController;
+  friend class CommentController;
 
-private:
-  /*
-   * Render field page
-   * Just print 'O que vc está pensando:' and return
-   */
-  Content field_content_page();
+  private:
+    /*
+     * Render field page
+     * Just print 'O que vc está pensando:' and return
+     */
+    Content field_content_page();
 
-public:
-  /*
-   * Render create comment page
-   * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-   */
-  void create_page();
-  /*
-   * Render show page
-   * Receives the id of comment and a boolean (if boolean true, show all comments)
-   */
-  void show_page(bool, unsigned int);
-  /*
-   * Render edit profile page
-   * Receives the id of comment
-   * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-   */
-  void edit_page(unsigned int);
-  /*
-   * Render edit profile page
-   * Receives the id of comment
-   * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-   */
-  void delete_page(unsigned int);
+  public:
+    /*
+     * Render create comment page
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void create_page();
+    /*
+     * Render show page
+     * Receives the id of comment and a boolean (if boolean true, show all comments)
+     */
+    void show_page(bool, unsigned int);
+    /*
+     * Render edit profile page
+     * Receives the id of comment
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void edit_page(unsigned int);
+    /*
+     * Render edit profile page
+     * Receives the id of comment
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void delete_page(unsigned int);
 };
 
 //------------------------------------------------
@@ -129,38 +129,38 @@ public:
  */
 class PostView {
 
-friend class PostController;
+  friend class PostController;
 
-private:
-  /*
-   * Render field page
-   * Just print 'O que vc está pensando:' and return
-   */
-  Content field_content_page();
+  private:
+    /*
+     * Render field page
+     * Just print 'O que vc está pensando:' and return
+     */
+    Content field_content_page();
 
-public:
-  /*
-   * Render create post page
-   * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-   */
-  void create_page();
-  /*
-   * Render show page
-   * Receives the id of post and a boolean (if boolean true, show all posts)
-   */
-  void show_page(bool, unsigned int);
-  /*
-   * Render edit profile page
-   * Receives the id of post
-   * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-   */
-  void edit_page(unsigned int);
-  /*
-   * Render edit profile page
-   * Receives the id of post
-   * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-   */
-  void delete_page(unsigned int);
+  public:
+    /*
+     * Render create post page
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void create_page();
+    /*
+     * Render show page
+     * Receives the id of post and a boolean (if boolean true, show all posts)
+     */
+    void show_page(bool, unsigned int);
+    /*
+     * Render edit profile page
+     * Receives the id of post
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void edit_page(unsigned int);
+    /*
+     * Render edit profile page
+     * Receives the id of post
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void delete_page(unsigned int);
 };
 
 //------------------------------------------------
@@ -176,49 +176,49 @@ public:
  */
 class UserView {
 
-friend class UserController;
+  friend class UserController;
 
-private:
-  /*
-   * Render field page
-   * Just print 'Preencha seu nome:' and return
-   */
-  Name field_name_page();
-  /*
-   * Render field page
-   * Just print 'Preencha seu email:' and return
-   */
-  Email field_email_page();
-  /*
-   * Render field page
-   * Just print 'Preencha sua senha:' and return
-   */
-  Password field_password_page();
+  private:
+    /*
+     * Render field page
+     * Just print 'Preencha seu nome:' and return
+     */
+    Name field_name_page();
+    /*
+     * Render field page
+     * Just print 'Preencha seu email:' and return
+     */
+    Email field_email_page();
+    /*
+     * Render field page
+     * Just print 'Preencha sua senha:' and return
+     */
+    Password field_password_page();
 
-public:
-	/*
-	 * Render sign up page
-	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-	 */
-	static void create_page();
-	/*
-	 * Render profile page
-	 * Receives the id of user
-	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-	 */
-	static void show_page();
-	/*
-	 * Render edit profile page
-	 * Receives the id of user
-	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-	 */
-	void edit_page(unsigned int);
-	/*
-	 * Render edit profile page
-	 * Receives the id of user
-	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-	 */
-	void delete_page(unsigned int);
+  public:
+    /*
+     * Render sign up page
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    static void create_page();
+    /*
+     * Render profile page
+     * Receives the id of user
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    static void show_page();
+    /*
+     * Render edit profile page
+     * Receives the id of user
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void edit_page(unsigned int);
+    /*
+     * Render edit profile page
+     * Receives the id of user
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */
+    void delete_page(unsigned int);
 };
 
 //------------------------------------------------
@@ -269,22 +269,22 @@ class AuthView {
 
   private:
     /// What?
-  	const static int SIM = 1;
+    const static int SIM = 1;
 
     /// What?
-  	const static int NAO = 2;
+    const static int NAO = 2;
 
   public:
-  	/*
-  	 * Renderiza tela de login
-  	 * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
-  	 */	
-  	static bool login_page();
+    /*
+     * Renderiza tela de login
+     * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
+     */ 
+    static bool login_page();
     /*
      * Renderiza tela de logout(so uma confimarcao pro usuario)
      * TODO: Existe um espaço entre as opções onde virão as mensagens de erro em vermelho.
      */
-  	static bool logout_page();
+    static bool logout_page();
 };
 
 //------------------------------------------------
